f78c1f06f2d347eb5abd15d66a95cd183991968d
Adam Silver <silver.inventor@gmail.com>
2021-07-21 16:15:48 -0400

    new com

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <AmplifySignOut />
-        <h2>Hi all new</h2>
+        <h2>Hi all new again</h2>
       </header>
     </div>
   );
